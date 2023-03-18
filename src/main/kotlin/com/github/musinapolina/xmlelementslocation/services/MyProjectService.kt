75535cb2fd634d3d4508aa1c7ba4500c4ccf3aa3
GitHub Action <action@github.com>
2023-03-18 23:09:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/musinapolina/xmlelementslocation/services/MyProjectService.kt
+++ src/main/kotlin/com/github/musinapolina/xmlelementslocation/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.musinapolina.xmlelementslocation.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.musinapolina.xmlelementslocation.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
